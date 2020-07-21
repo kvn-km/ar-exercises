3bfac14361064fb8413fa5350a11bd732cddb097
Kevin Kim <kevinfkim@gmail.com>
2020-07-21 04:28:27 +0000

    ex7 complete.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# 1
+# ../lib/store
+# ../lib/employee
+
+# 2
+print "Please enter a store name: "
+store_name = gets.chomp
+
+# 3
+@store100 = Store.create(name: store_name)
+# 4
+puts Store.where(name: "Toronto")
