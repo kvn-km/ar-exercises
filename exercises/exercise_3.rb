64948676460c702ad6e423b0c29d09fbdd58e02e
Kevin Kim <kevinfkim@gmail.com>
2020-07-20 23:09:54 -0400

    ex3 complete.

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,9 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: false, womens_apparel: false)
+puts Store.count
+
+Store.find_by(name: "Burnaby").destroy
+puts Store.count
